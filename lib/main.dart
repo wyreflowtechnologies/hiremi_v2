85aa377d89a94286c8f7bb51509013d0cfed0fa1
sameer15v3@gmail.com <98183896+Samillionaire@users.noreply.github.com>
2024-11-23 16:18:27 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,7 @@ class MyApp extends StatelessWidget {
         ),
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+
       ),
         
       
